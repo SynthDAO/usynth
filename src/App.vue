3409d4e5c1e6a687c11b7ef73b616825345689f4
Nour Haridy <nourharidy@yahoo.com>
2020-07-06 13:34:06 +0200

    Metamask integration

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,46 @@
 <template>
   <div class="container" id="app">
-    <NavBar/>
+    <NavBar
+    :authed="$store.state.authed"
+    :address="$store.state.address"
+    :network="$store.state.network"
+    @connect="connect"/>
     <router-view/>
   </div>
 </template>
 
 <script>
 import NavBar from "./components/NavBar"
+import {ethers} from 'ethers'
 export default {
   components:{
     NavBar
+  },
+  methods:{
+    async connect() {
+      await window.ethereum.enable()
+      const network = window.ethereum.networkVersion === '1'? 'Mainnet': window.ethereum.networkVersion === '42'? 'Kovan': undefined
+      if(network) {
+        window.ethereum.autoRefreshOnNetworkChange = true;
+        let wallet = (new ethers.providers.Web3Provider(window.ethereum)).getSigner()
+        this.$store.commit('auth', {
+          network,
+          address:window.ethereum.selectedAddress,
+          wallet
+        })
+      } else {
+        this.$buefy.dialog.alert({
+            title: 'Invalid Network',
+            message: 'Please switch to either Mainnet or Kovan',
+            type: 'is-danger',
+            hasIcon: true,
+            icon: 'times-circle',
+            iconPack: 'fa',
+            ariaRole: 'alertdialog',
+            ariaModal: true
+        })
+      }
+    }
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    authed:false,
+    address:"",
+    network:"",
+    wallet:{}
   },
   mutations: {
+    auth (state, {address, network, wallet}) {
+      state.address = address
+      state.network = network
+      state.wallet = wallet
+      state.authed = true
+    }
   },
   actions: {
   },
