f038baa0bfb9184d8840d641b2bca3b54fb75322
Nour Haridy <nourharidy@yahoo.com>
2020-07-14 06:14:55 +0200

    fix positions stories

--- stories/screens/0-Positions.stories.js
+++ stories/screens/0-Positions.stories.js
@@ -42,18 +42,18 @@ export const WithPositions = () => ({
       positions:[
         {
           name:"ETHBTC-AUG20",
-          amount:10000,
-          collateral:12000,
-          cratio:"4743.44%",
-          creq:"120%",
+          amount:"10000000000000000000000",
+          collateral:"12000000000000000000000",
+          cratio:4743.44,
+          creq:120,
           pending:"none",
         },
         {
           name:"DAIBTC-AUG20",
-          amount:1,
-          collateral:2000,
-          cratio:"200%",
-          creq:"150%",
+          amount:"10000000000000000000000",
+          collateral:"1000000000000000000000",
+          cratio:200,
+          creq:150,
           pending:"Withdrawal: 300 DAI",
         }
       ]
